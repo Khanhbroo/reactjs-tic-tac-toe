48e93c8ca0117d25a3837895e55728869d9986ab
Khanhbroo <khanhmathias532211@gmail.com>
2025-03-29 10:39:04 +0700

    Reduce state managements and identify unnecessary sate

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,25 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 
+const deriveActivePlayer = (gameTurns) => {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+};
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  // const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   const handleSelectSquare = (rowIndex, colIndex) => {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
@@ -27,21 +34,24 @@ function App() {
   };
 
   return (
-    <div id="game-container">
-      <ol id="players" className="highlight-player">
-        <Player
-          initialName="Player 1"
-          symbol="X"
-          isActive={activePlayer === "X"}
-        />
-        <Player
-          initialName="Player 2"
-          symbol="O"
-          isActive={activePlayer === "O"}
-        />
-      </ol>
-      <GameBoard turns={gameTurns} onSelectSquare={handleSelectSquare} />
-    </div>
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
+        </ol>
+        <GameBoard turns={gameTurns} onSelectSquare={handleSelectSquare} />
+      </div>
+      <Log turns={gameTurns} />
+    </main>
   );
 }
 
